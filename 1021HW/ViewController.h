17881d8b26ea5c8eb496886d01f781518eb7941b
Joseph Tsai <victoryandjoseph@gmail.com>
2014-10-21 16:42:35 +0800

    Initial Commit

---
+++ 1021HW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1021HW
+//
+//  Created by Joseph on 2014/10/21.
+//  Copyright (c) 2014年 dosomethingq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 1021HW/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  1021HW
+//
+//  Created by Joseph on 2014/10/21.
+//  Copyright (c) 2014年 dosomethingq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
